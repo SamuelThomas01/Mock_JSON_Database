af3aa2b68ea9f3a3d99cfbd080c34fe48897f512
SamuelThomas01 <samuelthomas0508@gmail.com>
2025-08-07 18:40:47 -0600

    update

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+const app = express();
+require('dotenv').config();
+import { connect } from 'require';
+const PORT = process.env.PORT;
+const uri = process.env.MONGO_URI;
+
+connect(uri).then(
+    async() =>{
+        console.log('COnnected to MongoDB.')
+
+        
+    
+
+app.listen(PORT, ()=>{
+    console.log(`Connected to port ${PORT}`);
+});
+}
+).catch((err) =>{ console.log(`Error: ${error}`)});
